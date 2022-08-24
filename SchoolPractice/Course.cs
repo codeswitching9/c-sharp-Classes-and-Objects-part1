c894e7b7b186512babd34f7ec206ccca1fbc9c32
codeswitchingstl <codeswitching@icloud.com>
2022-08-24 17:59:47 -0500

    apparently nothing I worked on actually pushed to git so hopefully this works this time

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        private string topic;
+        private string professor;
+        private List<Student> enrolledStudents;
+    }
+}
---
+++ SchoolPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SchoolPractice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student leslie = new Student("Leslie", 1, 1, 4.0);
+            Console.WriteLine(leslie.Name + " is a student.");
+        }
+    }
+}
